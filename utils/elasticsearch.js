fac4e6399eb0557c02fc06511b02ea3675a50520
godfery <157062357@qq.com>
2018-08-14 11:05:29 +0800

    change elasticsearch invoke rule

--- utils/elasticsearch.js
+++ utils/elasticsearch.js
@@ -1,55 +1,43 @@
 var elasticsearch = require('elasticsearch');
-var client = new elasticsearch.Client({
-    host: 'localhost:9200',
-    log: 'trace'
-});
-
-
-client.ping({
-    // ping usually has a 3000ms timeout
-    requestTimeout: 1000
-}, function (error) {
-    if (error) {
-        console.trace('elasticsearch cluster is down!');
-    } else {
-        console.log('All is well');
-    }
-});
 
 function fuzzyQuery(content, callback) {
+    var client = new elasticsearch.Client({
+        host: 'localhost:9200',
+        log: 'trace'
+    });
+    client.ping({
+        // ping usually has a 3000ms timeout
+        requestTimeout: 1000
+    }, function (error) {
+        if (error) {
+            console.trace('elasticsearch cluster is down!');
+        } else {
+            console.log('All is well');
+        }
+    });
     client.search(
         {
             index: 'test',
-            from:0,
-            size:40,
+            from: 0,
+            size: 40,
             q: content,
             body: {
-             
-            sort: [
-                {
-                    log_time: {
-                    "order" : "desc",
-                  }
-                }
-              ]
+
+                sort: [
+                    {
+                        log_time: {
+                            "order": "desc",
+                        }
+                    }
+                ]
             }
 
         }
-        //     {
-        //     index: 'twitter',
-        //     type: 'tweets',
-        //     body: {
-        //       query: {
-        //         match: {
-        //           body: 'elasticsearch'
-        //         }
-        //       }
-        //     }
-        //   }
+
         , function (err, response) {
             console.log(err);
-            if(!err) {
-                if(response.hits.total>0) {
+            if (!err) {
+                if (response.hits.total > 0) {
                     // for (const tweet of response.hits.hits) {
                     //     console.log('tweet:', tweet);
                     // }
@@ -60,9 +48,6 @@ function fuzzyQuery(content, callback) {
             } else {
                 callback(err, []);
             }
-            
-            
-            
             //   return response.hits.hits;
         }
     )
